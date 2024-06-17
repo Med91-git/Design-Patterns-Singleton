a694f1539d55b90e1acb26f10d5cdb3ec290dc65
Mederic <kottomouyema.m@gmail.com>
2024-06-17 12:23:50 +0200

    Quete Design Patterns Singleton

---
+++ DatabaseSingleton.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Design_Patterns___Singleton
+{
+    public class DatabaseSingleton
+    {
+        
+        private static DatabaseSingleton _instance;
+
+        private string _dataSource;
+        private string _dataBase;
+        private bool _integratedSecurity;
+        private string _userId;
+        private string _password;
+        private bool _encrypt;
+        private bool _trustServerCertificate;
+
+        private DatabaseSingleton() 
+        {
+            _dataSource = "localhost";
+            _dataBase = "ManageAccount";
+            _integratedSecurity = false;
+            _userId = "sa";
+            _password = "MedSQLserver91*";
+            _encrypt = true;
+            _trustServerCertificate = true;   
+        }
+
+        public static DatabaseSingleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new DatabaseSingleton();
+            }
+            return _instance;
+        }
+
+        public void Query(string sqlQuery) 
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Design_Patterns___Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+            ClassA classA = new ClassA(); 
+            ClassB classB = new ClassB();
+
+            classA.Database = DatabaseSingleton.GetInstance();
+            classB.Database = DatabaseSingleton.GetInstance(); 
+
+
+            classA.Database.Query("SELECT * FROM Persons");
+            classB.Database.Query("SELECT * FROM SavingAccounts");  
+
+
+        }
+    }
+}
